8015697b27bdd8090b65e708a7dde23bdaabdb1a
Daniel Schep <dschep@gmail.com>
2018-11-27 09:24:14 -0500

    recreate serverless/serverless#5522

---
+++ README.md
@@ -0,0 +1,6 @@
+# https://github.com/serverless/serverless/issues/5522
+```
+git clone https://github.com/dschep-bug-repos/sls-5522
+cd sls-5522
+sls invoke local -f hello
+```
--- handler.js
+++ handler.js
@@ -1,13 +1,17 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.hello = (event, context, callback) => {
+  setTimeout(
+    () =>
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Go Serverless v1.0! Your function executed successfully!',
+          input: event,
+        }),
+      }),
+    10,
+  );
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
